995ab4fcb8c71d6fd6c7b2854e6d2617298c7e08
SodooSodastreamSodny <chingunsodoo786@gmail.com>
2023-10-30 19:29:44 +0100

    0.0.1
    zacatky

--- DiscordBot/commands/TestCommands.cs
+++ DiscordBot/commands/TestCommands.cs
@@ -1,21 +1,32 @@
-﻿using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
+﻿    using DSharpPlus.CommandsNext;
+    using DSharpPlus.CommandsNext.Attributes;
+    using DSharpPlus.Entities;
 
-namespace DiscordBot.commands
-{
-    public class TestCommands : BaseCommandModule
+    namespace DiscordBot.commands
     {
-        [Command("test")]
-        public async Task MyFirstCommand(CommandContext ctx)
+        public class TestCommands : BaseCommandModule
         {
-            await ctx.Channel.SendMessageAsync($"CAu {ctx.User.Username}");
-        }
+            [Command("test")]
+            public async Task MyFirstCommand(CommandContext ctx)
+            {
+                await ctx.Channel.SendMessageAsync($"CAu {ctx.User.Username}");
+            }
 
 
-        [Command("add")]
-        public async Task Add(CommandContext ctx, int number1, int number2)
-        {
-            await ctx.Channel.SendMessageAsync($"Soucet je {number1 + number2}");
+            [Command("add")]
+            public async Task Add(CommandContext ctx, int number1, int number2)
+            {
+                await ctx.Channel.SendMessageAsync($"Soucet je {number1 + number2}");
+            }
+
+            [Command("embed")]
+            public async Task EmbedMessage(CommandContext ctx)
+            {
+                var message = new DiscordMessageBuilder()   
+                    .AddEmbed(new DiscordEmbedBuilder()
+                        .WithTitle("This is my first Discord Embed")
+                        .WithDescription($"This command was executed by {ctx.User.Username}"));
+                await ctx.Channel.SendMessageAsync(message);
+            }
         }
     }
-}
