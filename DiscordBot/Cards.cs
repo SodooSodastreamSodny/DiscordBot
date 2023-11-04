5056fa12696702934155c0ad00538f5f38a2cd6d
SodooSodastreamSodny <chingunsodoo786@gmail.com>
2023-11-04 20:41:00 +0100

    0.0.2
    stale pracuju na hre karet

--- DiscordBot/Cards.cs
+++ DiscordBot/Cards.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscordBot
+﻿namespace DiscordBot
 {
     public class CardSystem
     {
         public int[] cardNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         public string[] cardSuits = { "Clubs", "Spades", "Diamonds", "Hearts" };
+        public int SelectedNumber { get; set; }
+        public string SelectedCard { get; set; }
 
         public CardSystem() 
         {
             var random = new Random();
-            
+            int numberIndex = random.Next(0, cardNumbers.Length - 1);
+            int suitIndex = random.Next(0, cardSuits.Length - 1);
+            this.SelectedNumber = cardNumbers[numberIndex];
+            this.SelectedCard = $"{cardNumbers[numberIndex]} of {cardSuits[suitIndex]}";
         }
     }
 }
--- DiscordBot/commands/TestCommands.cs
+++ DiscordBot/commands/TestCommands.cs
@@ -1,43 +1,57 @@
 ﻿    using DSharpPlus.CommandsNext;
     using DSharpPlus.CommandsNext.Attributes;
     using DSharpPlus.Entities;
+using System.Globalization;
 
-    namespace DiscordBot.commands
-    {
-        public class TestCommands : BaseCommandModule
+namespace DiscordBot.commands
+{
+     public class TestCommands : BaseCommandModule
+     {
+        [Command("test")]
+        public async Task MyFirstCommand(CommandContext ctx)
         {
-            [Command("test")]
-            public async Task MyFirstCommand(CommandContext ctx)
-            {
-                await ctx.Channel.SendMessageAsync($"CAu {ctx.User.Username}");
-            }
+            await ctx.Channel.SendMessageAsync($"CAu {ctx.User.Username}");
+        }
 
 
-            [Command("add")]
-            public async Task Add(CommandContext ctx, int number1, int number2)
-            {
-                await ctx.Channel.SendMessageAsync($"Soucet je {number1 + number2}");
-            }
+        [Command("add")]
+        public async Task Add(CommandContext ctx, int number1, int number2)
+        {
+            await ctx.Channel.SendMessageAsync($"Soucet je {number1 + number2}");
+        }
 
-            [Command("embed")]
-            public async Task EmbedMessage(CommandContext ctx)
+        [Command("embed")]
+        public async Task EmbedMessage(CommandContext ctx)
+        {
+            var msg = new DiscordEmbedBuilder()
             {
-                var msg = new DiscordEmbedBuilder()
-                {
-                    Title = "This is my first Discord Embed",
-                    Description = $"This command was executed by {ctx.User.Username}",
-                    Color = DiscordColor.HotPink
-                };
-                    
-                await ctx.Channel.SendMessageAsync(embed: msg);
-            }
+                Title = "This is my first Discord Embed",
+                Description = $"This command was executed by {ctx.User.Username}",
+                Color = DiscordColor.HotPink
+            };
 
-            [Command("playCards")]
-            public async Task PlayCards(CommandContext ctx)
+            await ctx.Channel.SendMessageAsync(embed: msg);
+        }
+
+        [Command("playCards")]
+        public async Task PlayCards(CommandContext ctx)
+        {
+            var userCard = new CardSystem();
+            var botCard = new CardSystem();
+
+            var userCardEmbed = new DiscordEmbedBuilder()
             {
-                var symbols = new string[] {"clubs", "Spades", "Diamonds", "Hearts"};
-                
-            }
-            
+                Title = $"Your card is {userCard.SelectedCard}",
+                Color = DiscordColor.Green
+            };
+            var botCardEmbed = new DiscordEmbedBuilder()
+            {
+                Title = $"Bot's card is {botCard.SelectedCard}",
+                Color = DiscordColor.Red
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: userCardEmbed);
+            await ctx.Channel.SendMessageAsync(embed: botCardEmbed);
         }
     }
+}
